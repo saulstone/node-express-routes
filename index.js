e3d08dda78d59043d1b0d1ad48573dee5808dfbf
saulstone <pankajmundhra2016@gmail.com>
2019-04-11 16:09:59 +0530

    Express router

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
+/*
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type','text/plain');
@@ -32,7 +34,8 @@ app.put('/dishes', (req, res, next) => {
 app.delete('/dishes', (req,res,next) => {
   res.end('Deleting all the dishes!');
 });
-
+*/
+/*
 app.get('/dishes/:dishId', (req,res,next) => {
   res.end('Will send details of the dish: '
         +req.params.dishId + 'to you!');
@@ -54,8 +57,8 @@ app.delete('/dishes/:dishId', (req,res,next) => {
   res.end('Deleting the dish: '+ req.params.dishId);
 });
 
-
-
+*/
+app.use('/dishes',dishRouter);
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req, res, next) => {
